4c587dfc5f09ed403fb51a9d4fcd0e3d0593788e
codenamehenny <genesis09m@hotmail.com>
2024-07-11 12:08:14 -0400

    finding the largest and smallest number

--- TheGreatestShowdown.py
+++ TheGreatestShowdown.py
@@ -15,3 +15,15 @@ else:
     largest_number = third_number
 
 print("The largest number is" , largest_number)
+
+# Task 2: Identify the Smallest Improve upon your code from Task 1 to also determine the smallest number among the three and print it out.
+
+# comparing numbers to find smallest
+if first_number <= second_number and first_number <= third_number:
+    smallest_number = first_number
+elif second_number <= first_number and second_number <= third_number:
+    smallest_number = second_number
+else:
+    smallest_number = third_number
+
+print("The smallest number is", smallest_number)
